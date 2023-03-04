384dbcdb47731ebce7fa3fa6486a6200697a916e
endless.sine@gmail.com <endless.sine@gmail.com>
2023-03-04 15:38:50 +0200

    refactored

---
+++ src/lib.rs
@@ -0,0 +1,52 @@
+pub mod lib {
+    
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::time::Duration;
+use std::sync::mpsc::Receiver;
+
+/// Shared-State timer controlled by a channel
+/// 
+/// # Arguments 
+/// * `clock_pt` - a pointer to a variable containing your time 
+/// * `end_minutes` - amount of time to count down (in minutes) 
+/// * `rx` - a pointer to a mutex receiver channel
+///
+/// **TODO:**
+/// *Notifications*
+
+pub fn count_to(clock_pt: Arc<Mutex<Duration>>, end_minutes: u64, rx: Arc<Mutex<Receiver<&str>>>) {
+    let mut iter = 1;
+    let mut unlocked_clock = clock_pt.lock().unwrap();
+    let end_seconds = end_minutes * 60;
+    println!("starting a timer for {end_minutes} minutes");
+    let rx = rx.try_lock().unwrap();
+    while unlocked_clock.as_secs() < end_seconds {
+        let state = rx.try_recv().unwrap_or("");
+        match state {
+            "start" => {
+                iter = 1;
+            }
+            "pause" => {
+                iter = 0;
+            }
+            "quit" => {
+                return;
+            }
+            "show" => {
+                let time_output_minutes = (end_seconds - Duration::as_secs(&unlocked_clock)) / 60;
+                let time_output =
+                    end_seconds - Duration::as_secs(&unlocked_clock) - (time_output_minutes * 60);
+                println!("{time_output_minutes}:{time_output} left");
+            }
+            &_ => (),
+        }
+        *unlocked_clock += Duration::from_secs(iter);
+        std::thread::sleep(Duration::from_secs(1));
+        //        println!("{}", penis2.as_secs());
+    }
+    println!("Period Elapsed!");
+    *unlocked_clock = Duration::from_micros(0);
+}
+
+} /* Lib */
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
-use std::io::stdin;
+use std::io::{stdin, stdout};
 use std::process::exit;
 use std::sync::mpsc::{channel, Receiver};
 use std::sync::Arc;
 use std::sync::Mutex;
 use std::time::Duration;
+use lycopersicum::lib::count_to;
+
+
 
 const VER: &str = env!("CARGO_PKG_VERSION");
 
 fn main() {
+    // TODO: User IO
     println!("Welcome to lycopersicum! {VER}");
     let (tx, rx) = channel();
-    let args = vec![15, 5, 0];
+    let args = vec![30, 1, 1];
     let clock = Arc::new(Mutex::new(Duration::from_secs(0)));
     let rx = Arc::new(Mutex::new(rx));
     std::thread::spawn(move || loop {
@@ -23,6 +27,7 @@ fn main() {
         stdin().read_line(&mut input).unwrap();
         match input.trim() {
             "start" => tx.send("start").unwrap(),
+            "show" => tx.send("show").unwrap(),
             "pause" => tx.send("pause").unwrap(),
             "quit" => {
                 exit(0);
@@ -30,31 +35,4 @@ fn main() {
             _ => (),
         }
     }
-}
-
-fn count_to(clock_pt: Arc<Mutex<Duration>>, end: u64, rx: Arc<Mutex<Receiver<&str>>>) {
-    let mut iter = 1;
-    let mut unlocked_clock = clock_pt.lock().unwrap();
-    println!("{end}");
-    let rx = rx.try_lock().unwrap();
-    while unlocked_clock.as_secs() < end {
-        let state = rx.try_recv().unwrap_or("");
-        match state {
-            "start" => {
-                iter = 1;
-            }
-            "pause" => {
-                iter = 0;
-            }
-            "quit" => {
-                return;
-            }
-            &_ => (),
-        }
-        *unlocked_clock += Duration::from_secs(iter);
-        std::thread::sleep(Duration::from_secs(1));
-        //        println!("{}", penis2.as_secs());
-    }
-    println!("Period Elapsed!");
-    *unlocked_clock = Duration::from_micros(0);
 }
