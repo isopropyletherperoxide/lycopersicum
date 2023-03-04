c64f54e015c20f4257a04fbb9866630fb6b5e32d
endless.sine@gmail.com <endless.sine@gmail.com>
2023-03-04 12:00:02 +0200

    refactored

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io::stdin;
 use std::process::exit;
-use std::sync::mpsc::{Receiver, channel};
+use std::sync::mpsc::{channel, Receiver};
 use std::sync::Arc;
 use std::sync::Mutex;
 use std::time::Duration;
@@ -11,12 +11,11 @@ fn main() {
     println!("Welcome to lycopersicum! {VER}");
     let (tx, rx) = channel();
     let args = vec![15, 5, 0];
-    let bingo = Mutex::new(Duration::from_secs(0));
-    let bingo = Arc::new(bingo);
+    let clock = Arc::new(Mutex::new(Duration::from_secs(0)));
     let rx = Arc::new(Mutex::new(rx));
     std::thread::spawn(move || loop {
         for i in &args {
-            count_to(bingo.clone(), *i, rx.clone());
+            count_to(clock.clone(), *i, rx.clone());
         }
     });
     loop {
@@ -33,13 +32,13 @@ fn main() {
     }
 }
 
-fn count_to(penis: Arc<Mutex<Duration>>, end: u64, rx: Arc<Mutex<Receiver<&str>>>) {
+fn count_to(clock_pt: Arc<Mutex<Duration>>, end: u64, rx: Arc<Mutex<Receiver<&str>>>) {
     let mut iter = 1;
-    let mut penis2 = penis.lock().unwrap();
+    let mut unlocked_clock = clock_pt.lock().unwrap();
     println!("{end}");
     let rx = rx.try_lock().unwrap();
-    while penis2.as_secs() < end {
-        let state = rx.try_recv().unwrap_or(""); 
+    while unlocked_clock.as_secs() < end {
+        let state = rx.try_recv().unwrap_or("");
         match state {
             "start" => {
                 iter = 1;
@@ -52,10 +51,10 @@ fn count_to(penis: Arc<Mutex<Duration>>, end: u64, rx: Arc<Mutex<Receiver<&str>>
             }
             &_ => (),
         }
-        *penis2 += Duration::from_secs(iter);
+        *unlocked_clock += Duration::from_secs(iter);
         std::thread::sleep(Duration::from_secs(1));
         //        println!("{}", penis2.as_secs());
     }
     println!("Period Elapsed!");
-    *penis2 = Duration::from_micros(0);
+    *unlocked_clock = Duration::from_micros(0);
 }
